40655fe1f28f3007b3b3699640b0d8830557433b
Haylton Santos <aylton.souza10@gmail.com>
2025-09-22 14:06:52 +0100

    184 . Passamos dados pelos construtor da rota

--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
+import '../models/category.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
-  const CategoriesMealsScreen({super.key});
+
+  final Category caterogy;
+
+  const CategoriesMealsScreen(this.caterogy);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Receitas'),
+        title: Text(caterogy.title),
       ),
       body: Center(
-        child: Text('Receitas por Catergoria'),
+        child: Text('Receitas por Catergoria ${caterogy.id}'),
       ),
     );
   }
