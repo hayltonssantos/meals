71b9cea5d8da048e8ba9d6a61fb0012e2f46b411
Haylton Santos <aylton.souza10@gmail.com>
2025-10-27 13:44:32 +0000

    aula 191 - navegaçao entre telas

--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -1,16 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:meals/components/meal_item.dart';
+import 'package:meals/data/dummy_data.dart';
 import '../models/category.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
+
+
   @override
   Widget build(BuildContext context) {
     final caterogy = ModalRoute.of(context)!.settings.arguments as Category;
+
+    final categoryMeals = DUMMY_MEALS.where((meal){
+      return meal.categories.contains(caterogy.id);
+    }).toList();
     return Scaffold(
       appBar: AppBar(
         title: Text(caterogy.title),
       ),
       body: Center(
-        child: Text('Receitas por Catergoria ${caterogy.id}'),
+        child: ListView.builder(
+          itemCount: categoryMeals.length,
+          itemBuilder: (ctx, index){
+          return MealItem(categoryMeals[index]);
+        }),
       ),
     );
   }
